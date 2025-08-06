a87749a7e4c1779488027a64c3d38d7d7546496c
sumitwadhwa <sumitwadhwa.iitr@gmail.com>
2025-08-06 12:32:10 -0500

    Update models.py

--- app/models/models.py
+++ app/models/models.py
@@ -1,1 +1,16 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.core.database import Base
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    config = Column(String)
+
+class Pricing(Base):
+    __tablename__ = 'pricing'
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(Integer)
+    price = Column(Float)
 
