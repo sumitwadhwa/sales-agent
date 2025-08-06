5cf352d99fb7a6be83ef154cb5195a74abe37cf4
sumitwadhwa <sumitwadhwa.iitr@gmail.com>
2025-08-06 12:32:59 -0500

    Update schemas.py

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,1 +1,15 @@
+from pydantic import BaseModel
+
+class ProductSearchRequest(BaseModel):
+    keyword: str
+
+class ProductResponse(BaseModel):
+    id: int
+    name: str
+    description: str
+    config: str
+
+class PriceResponse(BaseModel):
+    product_id: int
+    price: float
 
