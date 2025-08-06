54f525c83254cd962627baedf97edc59fdbb916f
sumitwadhwa <sumitwadhwa.iitr@gmail.com>
2025-08-06 12:29:48 -0500

    Delete app/service.py

--- app/service.py
+++
@@ -1,8 +0,0 @@
-from . import repository
-from sqlalchemy.ext.asyncio import AsyncSession
-
-async def get_products(session: AsyncSession, keyword: str):
-    return await repository.fetch_products_by_keyword(session, keyword)
-
-async def get_price(session: AsyncSession, product_id: int):
-    return await repository.fetch_price_by_product_id(session, product_id)
